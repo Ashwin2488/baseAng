ce3812a5e0590b7c97abe52af445c03706f6409a
Ashwin2488 <ashwin06.s@gmail.com>
2014-08-09 00:54:07 +0530

    feat(): add documentation files

---
+++ js/docs-setup.js
@@ -0,0 +1,71 @@
+NG_DOCS={
+  "sections": {
+    "api": "API documentation",
+    "guide": "Guide"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "API Reference",
+      "type": "overview",
+      "moduleName": "API Reference",
+      "shortDescription": "proAngular generator - API Reference",
+      "keywords": "api generator guide overview proangular reference start yeoman"
+    },
+    {
+      "section": "guide",
+      "id": "index",
+      "shortName": "Guide",
+      "type": "overview",
+      "moduleName": "Guide",
+      "shortDescription": "Pro Angular Skeleton",
+      "keywords": "alt angular angularjs app application class code common config configurations coverage diagram es features file folder global google guide height http identify img integrated jasmine karma lang manage matchlist mode overview php png pollinterval pro proangular production prokarma recommended release runner scaffolds scalable service services servicetimeout serviceurl skeleton specific src staging structure test tests unit untested unused urls version width"
+    },
+    {
+      "section": "guide",
+      "id": "00_installation",
+      "shortName": "Installation",
+      "type": "overview",
+      "moduleName": "Installation",
+      "shortDescription": "Quick Start",
+      "keywords": "angular applications bower browser clone controllers ensure environment example existing git grunt-cli guide https install installation installed io javascript js library node nodejs npm org overview prerequisites pro-angular-yeoman quick repository running runtime sample server services skeleton start started written"
+    },
+    {
+      "section": "guide",
+      "id": "01_folder_structure",
+      "shortName": "Folder structure",
+      "type": "overview",
+      "moduleName": "Folder structure",
+      "shortDescription": "Folder structure",
+      "keywords": "alt app application assets bower build class cloned conf config configuration dependency dependent developing development diagram directory e2e easy efficient file files folder frameworks front-end grunt gruntfile guide height helper images img inside integration js json karma karma-e2e list manage modify modules node node_modules npm overview package png process project required scripts spent src stage structure styles testing tests time unit width"
+    },
+    {
+      "section": "guide",
+      "id": "02_application_structure",
+      "shortName": "Application structure",
+      "type": "overview",
+      "moduleName": "Application structure",
+      "shortDescription": "Angular structure guideline",
+      "keywords": "_test angular app application bar-directive_test complexity component components controller controllers conventions css datepicker definition depending describe detailed directive directives easily example existing file filename files filter filters folder follow foo-controller_test google group grouping guide guideline helpfull home-controller home-controller_test home-service html include including js logical lowercase maintain module modules named names naming object overview page1 page2 proposed purpose recommendations recommended scales service services specific structure style sub-section sub-sections tests tpl type ui unit units view"
+    },
+    {
+      "section": "guide",
+      "id": "03_adding_module",
+      "shortName": "Adding a Module",
+      "type": "overview",
+      "moduleName": "Adding a Module",
+      "shortDescription": "Adding a new Module",
+      "keywords": "$rootscope $routeprovider action add adding alert alert-info angular app application automatically base called class config configure content controller create creating css define destination extra file files folder function functionalities going grunt guide home-controller home-controller_test home-service html http injector inside js module modules navigate overview page1 page1ctrl page2 page3 page3-controller page3ctrl perfect project route running scripts setup skip sub-directory task template templateurl things tpl url view"
+    }
+  ],
+  "apis": {
+    "api": true,
+    "guide": false
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};

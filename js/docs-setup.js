18330a5d836295f4987c364a4fd46f4ea8455c29
Ashwin2488 <ashwin06.s@gmail.com>
2014-08-11 00:43:48 +0530

    chore(docs) :updated new content

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -14,13 +14,40 @@ NG_DOCS={
       "keywords": "api generator guide overview proangular reference start yeoman"
     },
     {
+      "section": "api",
+      "id": "proAngular.controller:RootCtrl",
+      "shortName": "RootCtrl",
+      "type": "controller",
+      "moduleName": "proAngular",
+      "shortDescription": "Base controller for the appication.Application specific global functionalities can be done here.",
+      "keywords": "$location $scope accessed actions active angularjs api app appication application base checks child configuring controller currently defaults fragment function functionalities global highlighting identifier isactive link module modules navigating org proangular provider required rootctrl selected specific url values viewlocation"
+    },
+    {
+      "section": "api",
+      "id": "proAngular.home",
+      "shortName": "proAngular.home",
+      "type": "overview",
+      "moduleName": "proAngular.home",
+      "shortDescription": "Home module with configuration and controller for home page",
+      "keywords": "api configuration controller module overview proangular"
+    },
+    {
+      "section": "api",
+      "id": "proAngular.home.controller:HomeCtrl",
+      "shortName": "HomeCtrl",
+      "type": "controller",
+      "moduleName": "proAngular.home",
+      "shortDescription": "Home controller that contains logic for home screen",
+      "keywords": "api controller logic proangular screen"
+    },
+    {
       "section": "guide",
       "id": "index",
       "shortName": "Guide",
       "type": "overview",
       "moduleName": "Guide",
-      "shortDescription": "Pro Angular Skeleton",
-      "keywords": "alt angular angularjs app application class code common config configurations coverage diagram es features file folder global google guide height http identify img integrated jasmine karma lang manage matchlist mode overview php png pollinterval pro proangular production prokarma recommended release runner scaffolds scalable service services servicetimeout serviceurl skeleton specific src staging structure test tests unit untested unused urls version width"
+      "shortDescription": "Angular Base Skeleton",
+      "keywords": "alt angular angularjs app application base class code common config configurations coverage diagram es features file folder global google guide height identify img integrated jasmine karma lang manage matchlist mode overview png pollinterval proangular production recommended release runner scaffolds scalable service services servicetimeout serviceurl skeleton specific src staging structure test tests unit untested unused urls version width"
     },
     {
       "section": "guide",
@@ -29,7 +56,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "Installation",
       "shortDescription": "Quick Start",
-      "keywords": "angular applications bower browser clone controllers ensure environment example existing git grunt-cli guide https install installation installed io javascript js library node nodejs npm org overview prerequisites pro-angular-yeoman quick repository running runtime sample server services skeleton start started written"
+      "keywords": "application applications bower browser clone controllers create environment example existing git grunt-cli guide https install installation installed io javascript js library local machine node nodejs npm org overview prerequisites pro-angular-yeoman quick repository running runtime server services skeleton start started written"
     },
     {
       "section": "guide",
@@ -38,7 +65,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "Folder structure",
       "shortDescription": "Folder structure",
-      "keywords": "alt app application assets bower build class cloned conf config configuration dependency dependent developing development diagram directory e2e easy efficient file files folder frameworks front-end grunt gruntfile guide height helper images img inside integration js json karma karma-e2e list manage modify modules node node_modules npm overview package png process project required scripts spent src stage structure styles testing tests time unit width"
+      "keywords": "alt app application assets assigned bootstrap bower build class conf config configuration defines definitions dependency dependent developing development diagram directive directory e2e easy efficient file files folder frameworks front-end grunt gruntfile guide height helper images img inside integration js json karma karma-e2e list manage manual modify module modules ng-app node node_modules npm overview package png process project required route scripts skeleton spent src stage structure styles testing tests time unit width"
     },
     {
       "section": "guide",
@@ -47,7 +74,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "Application structure",
       "shortDescription": "Angular structure guideline",
-      "keywords": "_test angular app application bar-directive_test complexity component components controller controllers conventions css datepicker definition depending describe detailed directive directives easily example existing file filename files filter filters folder follow foo-controller_test google group grouping guide guideline helpfull home-controller home-controller_test home-service html include including js logical lowercase maintain module modules named names naming object overview page1 page2 proposed purpose recommendations recommended scales service services specific structure style sub-section sub-sections tests tpl type ui unit units view"
+      "keywords": "_test admin adminpanelctrl angular app application bar-directive_test best camelcase cart collisions complexity component components controller controllers conventions css ctrl custom datepicker definition depending describe detailed directive directives easily example existing file filename files filter filters folder follow foo-controller_test functionality google group grouping guide guideline helpfull home-controller home-controller_test home-service homepage html include including indicating js libraries logical lowercamelcase lowercase maintain module modules named names namespacing naming nest object overview page1 page2 panel practices practises prefixes prevent proposed purpose recommendations recommended scales service services shopping shoppingcartctrl specific structure style sub-section sub-sections submodule substring tests third-party tpl type ui unit units uppercamelcase view"
     },
     {
       "section": "guide",
@@ -56,7 +83,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "Adding a Module",
       "shortDescription": "Adding a new Module",
-      "keywords": "$rootscope $routeprovider action add adding alert alert-info angular app application automatically base called class config configure content controller create creating css define destination extra file files folder function functionalities going grunt guide home-controller home-controller_test home-service html http injector inside js module modules navigate overview page1 page1ctrl page2 page3 page3-controller page3ctrl perfect project route running scripts setup skip sub-directory task template templateurl things tpl url view"
+      "keywords": "$rootscope $routeprovider action add adding alert alert-info angular app application automatically base called class config configure consider content controller create creating css define destination extra file files folder function functionalities going grunt guide home-controller home-controller_test home-service html http injector inside js module modules navigate overview page1 page1ctrl page2 page3 page3-controller page3ctrl perfect project route running scripts setup skip sub-directory task template templateurl things tpl url view"
     }
   ],
   "apis": {
@@ -65,6 +92,11 @@ NG_DOCS={
   },
   "html5Mode": false,
   "startPage": "/api",
+  "discussions": {
+    "shortName": "angularskeleton",
+    "url": "http://ashwin2488.github.io/pro-angular-yeoman",
+    "dev": false
+  },
   "scripts": [
     "angular.min.js"
   ]

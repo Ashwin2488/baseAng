9467ff73f83b336f0737c7def0dac8448a21d219
santhanam87 <sansandy04@gmail.com>
2014-08-12 15:52:29 +0530

    removing all files in remote branch

--- app/modules/page1/page1-directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('proAngular.page1').directive("matchList",function(){
-  return {
-     restrict: "AEC",
-     template: "<div ng-repeat='match in matchList'><p><span class='col-xs-6'>{{match.team1}} Vs {{match.team2}}</span><span>{{match.result}}</span></p></div>",
-     link: function(scope,element){
-     }
-  }   
-});
